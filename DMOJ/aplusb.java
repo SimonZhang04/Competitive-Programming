d51ed316334e171fcc3f1a242b08e1cc2f55c7fd
Simon Zhang <77755696+SimonZhang04@users.noreply.github.com>
2021-03-20 14:38:49 -0400

    Create aplusb.java

---
+++ DMOJ/aplusb.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+
+            int N = in.nextInt();
+            for (int i = 0; i < N; i++) {
+                int a = in.nextInt();
+                int b = in.nextInt();
+                System.out.println(a + b);
+            }
+        }
+    }
